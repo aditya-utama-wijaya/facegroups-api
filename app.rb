5072541de74678d07300944be6f9c86a098438e5
adi-botak- <adityautamawijaya@gmail.com>
2016-11-02 20:12:41 +0800

    Simplify econfig setup

--- app.rb
+++ app.rb
@@ -3,39 +3,36 @@ require 'sinatra'
 require 'econfig'
 require 'facegroups'
 
-ENV['RACK_ENV'] ||= 'development'
-Econfig.env = ENV['RACK_ENV']
-Econfig.root = File.dirname(__FILE__)
-
-module Groupie
+# GroupieAPI web service
+class GroupieAPI < Sinatra::Base
   extend Econfig::Shortcut
 
-  # GroupieAPI web service
-  class API < Sinatra::Base
-    API_VER = 'v0.1'
+  Econfig.env = settings.environment.to_s
+  Econfig.root = settings.root
+
+  API_VER = 'v0.1'
   
-    get '/?' do
-      "GroupieAPI latest version endpoints are at: /#{API_VER}/"
-    end
+  get '/?' do
+    "GroupieAPI latest version endpoints are at: /#{API_VER}/"
+   end
 
-    get "/#{API_VER}/group/:fb_group_id/?" do
-      group = FaceGroups::Group.find(id: params[:fb_group_id])
+  get "/#{API_VER}/group/:fb_group_id/?" do
+     group = FaceGroups::Group.find(id: params[:fb_group_id])
 
-      { group_id: group.id, name: group.name }.to_json
-    end
+     { group_id: group.id, name: group.name }.to_json
+  end
 
-    get "/#{API_VER}/group/:fb_group_id/feed/?" do
-      group = FaceGroups::Group.find(id: params[:fb_group_id])
+   get "/#{API_VER}/group/:fb_group_id/feed/?" do
+    group = FaceGroups::Group.find(id: params[:fb_group_id])
 
-      {
-        feed: group.feed.postings.map do |post|
-          {
-            posting: {
-              posting_id: post.id
-            }
+     {
+      feed: group.feed.postings.map do |post|
+        {
+          posting: {
+            posting_id: post.id
           }
-        end
-      }.to_json
-    end
+        }
+      end
+     }.to_json
   end
 end
