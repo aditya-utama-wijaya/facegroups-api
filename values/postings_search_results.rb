6f15605343f8c4cfb27b6c715ae32c5841f1d01a
adi-botak- <adityautamawijaya@gmail.com>
2016-12-02 21:43:53 +0800

    new route ro get any updates on Facebook for a group

--- values/postings_search_results.rb
+++ values/postings_search_results.rb
@@ -1,3 +1,3 @@
 # frozen_string_literal: true
 
-PostingsSearchResults = Struct.new :search_term_used, :group_id, :postings
+PostingsSearchResults = Struct.new :group_id, :postings, :search_term_used
